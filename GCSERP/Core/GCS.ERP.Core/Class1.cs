965a87d36bb8a0d42ae1f8ecc6e99cfd2309268c
Welisson Fabricio <welisson.listas@gmail.com>
2021-06-21 20:48:40 -0300

    Criação da Solução Principal Criação da API de Identidade Criação do MVC da Identidade Criação da Biblioteca Core

---
+++ GCSERP/Core/GCS.ERP.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GCS.ERP.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ GCSERP/IU/GCS.ERP.Identidade.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GCS.ERP.Identidade.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
